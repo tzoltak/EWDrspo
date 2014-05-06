61aa645384be25fb77406e4481ea5983cda5fb65
tzoltak <t.zoltak@ibe.edu.pl>
2014-05-07 01:19:09 +0200

    Drobne poprawki w sprawdz_baze().

--- R/sprawdz_baze.R
+++ R/sprawdz_baze.R
@@ -14,12 +14,12 @@ sprawdz_baze = function(baza) {
   if (!is.numeric(idRspo)) stop("Oczekiwano, że kolumna z id RSPO będzie typu numeric.")
   if (any(is.na(idRspo))) stop("\nBraki id_rspo w wierszach:\n", paste0("   ", rownames(baza)[is.na(idRspo)], "\n"))
   if (length(unique(idRspo)) != length(idRspo)) stop("\nZduplikowane id_rspo:\n", paste0("   ", idRspo[duplicated(idRspo)], "\n"))
-  idRspoNadrz = sort(unique(na.omit(baza[, grepl("^(numer|id).rspo.(jednostki.nadrzędnej|jedn.nadrz)$", tolower(names(baza)))])))
+  idRspoNadrz = sort(unique(na.omit(baza[, grepl("^(numer|id).rspo.(jednostki.nadrzędnej|jednostki.nadrzednej)$", tolower(names(baza)))])))
   if (is.null(idRspoNadrz)) stop("Nie znaleziono kolumny z id RSPO jednostki nadrzędnej.")
   if (!is.numeric(idRspoNadrz)) stop("Oczekiwano, że kolumna z id RSPO jednostki nadrzędnej będzie typu numeric.")
   brakIdRspoNadrz = idRspoNadrz[!(idRspoNadrz%in%idRspo)]
   if (length(brakIdRspoNadrz) > 0) {
-    liczbyBrakIdRspoNadrz = sapply(brakIdRspoNadrz, function(x, idRspoNadrz) return(sum(idRspoNadrz == x)), idRspoNadrz=idRspoNadrz)
+    liczbyBrakIdRspoNadrz = sapply(brakIdRspoNadrz, function(x, idRspoNadrz) return(sum(idRspoNadrz == x)), idRspoNadrz=na.omit(baza[, grepl("^(numer|id).rspo.(jednostki.nadrzędnej|jednostki.nadrzednej)$", tolower(names(baza)))]))
     stop("\nBrak rekordów o jednostkach nadrzędnych o id RSPO równych:\n", paste0("   ", idRspoNadrz[ !(idRspoNadrz%in%idRspo)], " (", liczbyBrakIdRspoNadrz," odnosząca/e/ych się jednostka/i/ek)\n"))
   }
   invisible(NULL)
